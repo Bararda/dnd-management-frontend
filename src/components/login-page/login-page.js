78aa70e63af8e1ae890e45094fa8d8976b6b6693
Bararda <37812842+Bararda@users.noreply.github.com>
2020-01-09 21:12:54 -0800

    repo initializations and some generic components

---
+++ src/components/login-page/login-page.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Form from "react-bootstrap/Form";
+function LoginPage(props) {
+    return (
+        <Form>
+            <Form.Group controlId="loginUsername">
+                <Form.Label>Username</Form.Label>
+                <Form.Control type="text" placeholder="Username"/>
+            </Form.Group>
+
+            <Form.Group controlId="loginPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password"/>
+            </Form.Group>
+        </Form>
+    );
+}
+
+export default LoginPage
---
+++ src/core/nav-bar/nav-bar.js
@@ -0,0 +1,28 @@
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import React from "react";
+import "./nav-bar.css";
+function NavBar(props) {
+    return (
+        <Navbar className="nav-bar">
+            <NavLogo><img src={require("./dnd_logo.svg")} className="nav-logo" alt="dnd"/></NavLogo>
+            <NavItem href="#home">Home</NavItem>
+        </Navbar>
+    );
+}
+
+function NavItem(props) {
+    return (
+        <Nav.Item>
+            <Nav.Link href={props.href}>{props.children}</Nav.Link>
+        </Nav.Item>
+    );
+}
+
+function NavLogo(props) {
+    return (
+        <Navbar.Brand classList="" href={props.href}>{props.children}</Navbar.Brand>
+    )
+}
+
+export default NavBar;
