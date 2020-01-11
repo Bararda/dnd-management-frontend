dc1a4baaf932c74ce762a1fe6e8532db2df52838
Anthony <anthony.doucet@northorca.com>
2020-01-10 16:07:49 -0800

    moved icon and adeed button to login form for remember me

--- src/components/login-page/login-page.js
+++ src/components/login-page/login-page.js
@@ -7,9 +7,12 @@ function LoginPage(props) {
                 <Form.Label>Username</Form.Label>
                 <Form.Control type="text" placeholder="Username"/>
             </Form.Group>
-
             <Form.Group controlId="loginPassword">
                 <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password"/>
+            </Form.Group>
+            <Form.Group controlId="loginRememberMe">
+                <Form.Label>Remember Me</Form.Label>
                 <Form.Control type="password" placeholder="Password"/>
             </Form.Group>
         </Form>
